fb7b5868026ad0c31e5f721cd39a16c83282d9e3
VanshJohari29 <vansh2039@gmail.com>
2023-05-13 12:49:16 +0530

    changes in java script for the login page

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Perform login authentication
     if (username === "admin" && password === "password") {
         window.location.href = "dashboard.html";
-<<<<<<< HEAD
     }
         else if(username === "fabrication" && password === "fab1"){
                window.location.href = "fabrication.html";
@@ -20,19 +19,6 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
           window.location.href = "assembly.html";
         }
      else {
-=======
-    } 
-    else if (username === "assembly" && password === "password") {
-      window.location.href = "assembly.html";
-  } 
-  else if (username === "subassembly" && password === "password") {
-    window.location.href = "subassembly.html";
-} 
-else if (username === "fabrication" && password === "password") {
-  window.location.href = "fabrication.html";
-} 
-    else {
->>>>>>> 25686b2ba2f9d657aa47a719063f5369d5ba2a7d
       document.getElementById("message").innerHTML = "Invalid username or password!";
     }
   });
