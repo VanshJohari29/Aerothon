c12afe5b2dadd125e508a1a9bb39509791166e6b
VanshJohari29 <vansh2039@gmail.com>
2023-05-13 12:46:38 +0530

    changes in dashboard

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Perform login authentication
     if (username === "admin" && password === "password") {
         window.location.href = "dashboard.html";
-    } else {
+    }
+        else if(username === "fabrication" && password === "fab1"){
+               window.location.href = "fabrication.html";
+        }
+        else if(username === "subassembly" && password === "subassembly1")
+        {
+            window.location.href = "subassembly.html";
+        }
+        else if(username === "assembly" && password === "assembly1"){
+          window.location.href = "assembly.html";
+        }
+     else {
       document.getElementById("message").innerHTML = "Invalid username or password!";
     }
   });
