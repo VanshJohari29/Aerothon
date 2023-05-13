828f522d816074902ae0fbf0581d719e0fc05c56
Saumya Bharti <bhartisaumya22@gmail.com>
2023-05-13 12:17:25 +0530

    added role

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Perform login authentication
     if (username === "admin" && password === "password") {
         window.location.href = "dashboard.html";
-    } else {
+    } 
+    else if (username === "assembly" && password === "password") {
+      window.location.href = "assembly.html";
+  } 
+  else if (username === "subassembly" && password === "password") {
+    window.location.href = "subassembly.html";
+} 
+else if (username === "fabrication" && password === "password") {
+  window.location.href = "fabrication.html";
+} 
+    else {
       document.getElementById("message").innerHTML = "Invalid username or password!";
     }
   });
